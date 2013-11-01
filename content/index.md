0586210d857dec0bdd11598af3b29ebefec3a11b
zenry <henry@postb.us>
2013-11-01 11:11:35 +0100

    update

--- content/index.md
+++ content/index.md
@@ -1,4 +1,16 @@
 template: page
-title: welcome
+title: Welcome
+list:
+  - item 1
+  - item 2
+  - item 3
 ---
 This is the **index** page
+
+{{ metadata.title|title }}
+
+{{metadata.time}}
+
+{% for item in metadata.list %}
+- {{ item }}
+{% endfor %}
--- web/index.php
+++ web/index.php
@@ -11,6 +11,19 @@ require __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application;
 
 /**
+ * Settings
+ */
+$app['settings'] = [
+	'path'        => 'index',
+	'content_ext' => '.md',
+	'tpl_ext'     => '.html.twig',
+	'tpl_std'     => 'page',
+	'cache'       => 60,
+	'views'       => 'views',
+	'content'     => 'content'
+];
+
+/**
  * Memcache
  */
 $app['memcache.store'] = $app->share(function() {
@@ -35,22 +48,20 @@ $app['symfony.yaml.parser'] = $app->share(function () {
 });
 
 /**
- * Twig
+ * Twig Loader File
  */
 $app->register(new Silex\Provider\TwigServiceProvider, [
-	'twig.path' => __DIR__.'/../views',
+	'twig.path' => [
+		__DIR__.'/../'.$app['settings']['views']
+	],
 ]);
 
 /**
- * Settings
+ * Twig Loader String
  */
-$app['settings'] = [
-	'path'        => 'index',
-	'content_ext' => '.md',
-	'tpl_ext'     => '.html.twig',
-	'tpl_std'     => 'page',
-	'cache'       => 60
-];
+$app['twig.string'] = $app->share(function() {
+	return new Twig_Environment(new Twig_Loader_String);
+});
 
 /**
  * Main route
@@ -60,14 +71,14 @@ $app->get('{path}', function($path) use($app)
 	// Generate key for cache storage
 	$key = md5($path);
 
-	// Check cache for Response object
-	$response = $app['memcache.store']->get($key);
+	// Check page is cached
+	$html = $app['memcache.store']->get($key);
 
-	// No cached Response found
-	if(empty($response))
+	// No cached html found
+	if(empty($html))
 	{
 		// Make path to file on disk
-		$file = __DIR__.'/../content/'.$path.$app['settings']['content_ext'];
+		$file = __DIR__.'/../'.$app['settings']['content'].'/'.$path.$app['settings']['content_ext'];
 
 		// Return error if file is not found
 		if( ! is_file($file)) {
@@ -77,30 +88,32 @@ $app->get('{path}', function($path) use($app)
 			]);
 		}
 
-		// Read contents of file
-		$data = file_get_contents($file);
-
 		// Split file into metadata and content
-		list($metadata, $content) = preg_split('/\s+-{3,}\s+/', $data, 2, PREG_SPLIT_NO_EMPTY);
+		list($metadata, $content) = preg_split('/\s+-{3,}\s+/', file_get_contents($file), 2, PREG_SPLIT_NO_EMPTY);
 
 		// Parse metadata to array
 		$metadata = $app['symfony.yaml.parser']->parse($metadata);
 
-		// Parse content to HTML
-		$content  = $app['dflydev.markdown']->transformMarkdown($content);
+		// Create html object with template and data
+		$html = $app['twig']->render(
 
-		// Check if template is set in metadata, fallback to default if not
-		$template = isset($metadata->template) ? $metadata.$app['settings']['tpl_ext'] : $app['settings']['tpl_std'].$app['settings']['tpl_ext'];
+			// Check is template is set, load default if missing
+			(isset($metadata['template']) ? $metadata['template'].$app['settings']['tpl_ext'] : $app['settings']['tpl_std'].$app['settings']['tpl_ext']), [
 
-		// Create Response object with template and data
-		$response = $app['twig']->render($template, compact('metadata', 'content'));
+				// Pass metadata to template
+				'metadata' => $metadata,
 
-		// Store Response object in cache by key
-		$app['memcache.store']->set($key, $response, MEMCACHE_COMPRESSED, ($app['debug'] ? 1 : $app['settings']['cache']));
+				// Render content with Twig Loader String and parse to HTML with MarkdownExtraParser
+				'content' => $app['dflydev.markdown']->transformMarkdown($app['twig.string']->render($content, compact('metadata')))
+			]
+		);
+
+		// Store html in cache by key
+		$app['memcache.store']->set($key, $html, MEMCACHE_COMPRESSED, ($app['debug'] ? 1 : $app['settings']['cache']));
 	}
 
-	// Return Response object
-	return $response;
+	// Return html
+	return $html;
 
 
 })->value('path', $app['settings']['path'])->assert('path', '.+');
